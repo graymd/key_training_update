c34c98fa6c657dbd6e93a2d175ee3eec186c4437
Malachi Gray <graymd.cfi@gmail.com>
2016-12-16 16:58:26 -0500

    create Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css'
+
+class Navbar extends React.Component {
+
+  render() {
+    return (
+      <h1>Navbar Here</h1>
+    )
+  }
+}
+
+export default Navbar;
